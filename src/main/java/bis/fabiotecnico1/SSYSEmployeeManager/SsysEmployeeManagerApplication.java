2d445c5ea6b75e8bfbe5db15e8d7e79d8d0b16d0
Fabio Luis <ml@fabiotecnico1.biz>
2022-04-03 23:58:55 -0300

    Maven Project Created

---
+++ src/main/java/bis/fabiotecnico1/SSYSEmployeeManager/SsysEmployeeManagerApplication.java
@@ -0,0 +1,13 @@
+package bis.fabiotecnico1.SSYSEmployeeManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsysEmployeeManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsysEmployeeManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bis/fabiotecnico1/SSYSEmployeeManager/SsysEmployeeManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package bis.fabiotecnico1.SSYSEmployeeManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsysEmployeeManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
