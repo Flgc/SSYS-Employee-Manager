51933ade0675f9c311c1d9d06f7bd9b8919d7bdf
Fabio Luis <ml@fabiotecnico1.biz>
2022-04-04 22:24:04 -0300

    SSYS-Employee-Manager - H2 Database Configuration

---
+++ src/main/java/bis/fabiotecnico1/SSYSEmployeeManager/entities/Employee.java
@@ -0,0 +1,84 @@
+package bis.fabiotecnico1.SSYSEmployeeManager.entities;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_employees")
+public class Employee {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String name;
+	private String email;
+	private String department;
+	private Double salary;
+	private LocalDate birth_date;
+
+	public Employee() {		
+	}
+
+	public Employee(long id, String name, String email, String department, Double salary, LocalDate birth_date) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.department = department;
+		this.salary = salary;
+		this.birth_date = birth_date;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+
+	public LocalDate getBirth_date() {
+		return birth_date;
+	}
+
+	public void setBirth_date(LocalDate birth_date) {
+		this.birth_date = birth_date;
+	}	
+	
+}
